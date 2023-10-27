3e820b9b84c1a640cd42a551689ad7d65581e900
Zarplata-TV <zarplata2021@gmail.com>
2023-10-27 22:41:29 +0700

    Добавлена система Pool и скрипт выстрела v1.1
    Добавлен дамаг

--- ProjectHalloweenJam/Assets/Scripts/Bullet/Bullet.cs
+++ ProjectHalloweenJam/Assets/Scripts/Bullet/Bullet.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    [SerializeField] private int _damage;
     private void OnTriggerEnter2D(Collider2D other)
     {
+        if (other.TryGetComponent<IDamageable>(out var damageable))
+        {
+            damageable.TryTakeDamage(_damage);
+        }
         if (other.tag == "Wall") 
         {
             transform.position = new Vector3(0, 0, -100);
             this.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
             this.gameObject.SetActive(false);
         }
+        
     }
 }
