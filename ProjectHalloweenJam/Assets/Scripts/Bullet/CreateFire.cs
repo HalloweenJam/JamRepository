bf4e8ce42238313e914307028091657a7493dd16
Zarplata-TV <zarplata2021@gmail.com>
2023-10-28 15:25:14 +0700

    с

--- ProjectHalloweenJam/Assets/Scripts/Bullet/CreateFire.cs
+++ ProjectHalloweenJam/Assets/Scripts/Bullet/CreateFire.cs
@@ -1,64 +1,88 @@
+using Player.Controls;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Rendering;
 using UnityEngine;
+using Weapons;
+
+[Serializable]
+public struct BulletInfo
+{
+    public enum BulletType
+    {
+        line, sin, circle, fraction, firework,
+    }
+    public BulletType Type;
+    public Sprite Sprite;
+    public int Damage;
+    public float Speed;
+    public float Size;
+    public float TimeHeart;
+    public int Coint;
+    public int Radius;
+
+}
 
 public class CreateFire : MonoBehaviour
 {
-    public GameObject firePrefab;
-    public float Speed;
-    public int StartCount = 50;
-    public int AddCount = 10;
+    [SerializeField] private GameObject firePrefab;
+    [SerializeField] private float Speed;
+    [SerializeField] private int StartCount = 50;
+    [SerializeField] private int AddCount = 10;
+
+    Vector3 _worldMousePosition;
+
+    
+
+    [SerializeField]
+    private List<BulletInfo> _bullets;
 
 
     Transform bullet = null;
-
+    InputReader _inputReader;
 
     private void Start()
     {
         CreatePrefab(StartCount);
+        /*_inputReader = InputReaderManager.Instance.GetInputReader();
+        _inputReader.MousePosition += MousePosition;
+        _inputReader.ShootingEvent += ShootingEvent;*/
     }
-    // Update is called once per frame
-    /*void Update()
+
+    /*private void ShootingEvent()
     {
-        if ( firePrefab != null)
-        {
-            if (Input.GetMouseButtonDown(0))
-            {
-                Transform bullet = null;
-                    for (int i = 0; i < transform.childCount; i++)
-                    {
-                        if (!transform.GetChild(i).gameObject.activeSelf)
-                        {
-                            bullet = transform.GetChild(i);
-                            goto Continue;
-                        }
-                       
-                    }
-                    bullet = CreatePrefab(AddCount).transform;
+        OnShooting(1, new Vector3(0, 0, 0), _worldMousePosition);
+    }
 
-                    Continue:
-                    try
-                    {
-                        bullet.gameObject.SetActive(true);
-                        bullet.transform.position = new Vector3(0, 0, 0);
-
-                        Vector3 diference = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                        Vector3 aimDirection = (diference - bullet.position).normalized;
-                        float rotateZ = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
-                        bullet.eulerAngles = new Vector3(0f, 0f, rotateZ);
-
-                        bullet.GetComponent<Rigidbody2D>().velocity = bullet.right * Speed * Time.fixedDeltaTime;
-                    }
-                    catch (System.Exception)
-                    {
-                        Debug.LogError("No Bullet");
-                    }      
-            }
-        }
+    private void MousePosition(Vector2 vector)
+    {
+        _worldMousePosition = Camera.main.ScreenToWorldPoint(vector);
     }*/
 
-    public void Shooting(Vector3 startPosition, Vector3 direction)
+    public void OnShooting(int index, Vector3 startPosition, Vector3 direction)
+    {
+        switch(_bullets[index].Type) 
+        {
+            case BulletInfo.BulletType.line:
+                Shooting(_bullets[index], startPosition, direction);
+                break;
+            case BulletInfo.BulletType.sin:
+                break;
+            case BulletInfo.BulletType.circle:
+                CircleShooting(_bullets[index], startPosition, direction);
+                break;
+            case BulletInfo.BulletType.fraction:
+                break;
+            case BulletInfo.BulletType.firework:
+                break;
+            default:
+                break;
+        }
+    }
+
+
+    void Shooting(BulletInfo bulletInfo,Vector3 startPosition, Vector3 direction)
     {
         if (firePrefab != null)
         {
@@ -72,7 +96,7 @@ public class CreateFire : MonoBehaviour
 
             }
             bullet = CreatePrefab(AddCount).transform;
-
+            Shooting(bulletInfo, startPosition, direction);
         Continue:
             try
             {
@@ -83,13 +107,36 @@ public class CreateFire : MonoBehaviour
                 Vector3 aimDirection = (direction - bullet.position).normalized;
                 float rotateZ = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
                 bullet.eulerAngles = new Vector3(0f, 0f, rotateZ);
-
-                bullet.GetComponent<Rigidbody2D>().velocity = bullet.right * Speed * Time.fixedDeltaTime;
+                bullet.GetComponent<Bullet>().OnAttack(bulletInfo, BulletInfo.BulletType.line);
+                //bullet.GetComponent<Rigidbody2D>().velocity = bullet.right * Speed * Time.fixedDeltaTime;
             }
             catch (System.Exception)
             {
                 Debug.LogError("No Bullet");
             }
+        }
+    }
+
+    void CircleShooting(BulletInfo bulletInfo, Vector3 startPosition, Vector3 direction)
+    {
+        if (bulletInfo.Coint == 0)
+        {
+            bulletInfo.Coint = 1;
+        }
+        if (bulletInfo.Radius == 0)
+        {
+            bulletInfo.Radius = 1;
+        }
+        float anlgeCoint = 360 / bulletInfo.Coint;
+        float angle = 0;
+        float positionX;
+        float positionY;
+        for (int i = 0; i < bulletInfo.Coint; i++)
+        {
+            positionX = startPosition.x + Mathf.Cos((angle * Mathf.PI) / 180) * bulletInfo.Radius;
+            positionY = startPosition.y + Mathf.Sin((angle * Mathf.PI) / 180) * bulletInfo.Radius;
+            Shooting(bulletInfo, startPosition, new Vector3(positionX, positionY, 0));
+            angle += anlgeCoint;
         }
     }
 
---
+++ ProjectHalloweenJam/Assets/Scripts/Core/Interfaces/IDamageable.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.Interfaces
+{
+    public interface IDamageable
+    {
+       // void OnLine(BulletInfo info);
+        public bool TryTakeDamage(int damage, bool instantKill = false, bool ignoreInvisibility = false);
+    }
+}
