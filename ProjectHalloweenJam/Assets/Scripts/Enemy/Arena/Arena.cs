98fa77839f0cfab8081085620ebdd23b51bac1ac
6izmo <happyboy3421@gmail.com>
2023-10-28 18:15:26 +0300

    что0то

---
+++ ProjectHalloweenJam/Assets/Scripts/Enemy/Arena/Arena.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class Arena : MonoBehaviour
+{
+    [SerializeField] private SpawnerEnemy _spawnerEnemy;
+    [SerializeField] private Transform _walls;
+
+
+    public int _countEnemy;
+    private bool _canSpawning = true;
+    private bool _enemyEmpty = false;
+
+    private void Awake()
+    {
+        // EnemyStats.OnDeath += (pos) => _countEnemy--;
+    }
+
+    public void ActivateArena()
+    {
+       // _spawnerEnemy.SpawnEnemy(this);
+        _countEnemy = _spawnerEnemy.transform.childCount;
+
+        foreach (Transform wall in _walls)       
+            wall.gameObject.SetActive(true);    
+    }
+}
---
+++ ProjectHalloweenJam/Assets/Scripts/Player/PlayerInteract.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Core.Interfaces;
+using Managers;
+using Player.Controls;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace Player
+{
+    [RequireComponent(typeof(Inventory))]
+    public class PlayerInteract : MonoBehaviour
+    {
+        [SerializeField] private float _range = 1.5f;
+        [SerializeField] private LayerMask _interactionsLayerMask;
+
+        [SerializeField, HideInInspector] private Inventory _inventory;
+        
+        private InputReader _inputReader;
+
+        private void OnValidate()
+        {
+            _inventory = GetComponent<Inventory>();
+        }
+
+        private void Start()
+        {
+            _inputReader = InputReaderManager.Instance.GetInputReader();
+
+            _inputReader.InteractEvent += Interact;
+        }
+
+        private void Interact()
+        {
+            var overlap = Physics2D.OverlapCircle(transform.position, _range, _interactionsLayerMask);
+
+            if (!overlap)
+                return;
+            
+            if (overlap.TryGetComponent<IInteractable>(out var interactable))
+                interactable.Interact(_inventory);
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(transform.position, _range);
+        }
+    }
+}
