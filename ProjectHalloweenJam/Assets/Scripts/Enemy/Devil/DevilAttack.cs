05a496c0a93d3c329fb809a594f2d7b5741f6d2a
6izmo <happyboy3421@gmail.com>
2023-10-29 02:14:05 +0300

    cum it

--- ProjectHalloweenJam/Assets/Scripts/Enemy/Devil/DevilAttack.cs
+++ ProjectHalloweenJam/Assets/Scripts/Enemy/Devil/DevilAttack.cs
@@ -4,20 +4,22 @@ using System.Collections;
 public class DevilAttack : EnemyAttack
 {
     private string _attackName = "Attack";
+    private bool _canAttack = true;
     private bool _isAttack = false;
 
     public bool IsAttack => _isAttack;
 
     public override void Attack()
     {
-        StartCoroutine(AttackCor());
+        if(!_isAttack && _canAttack)
+            StartCoroutine(AttackCor());
     }
 
     private IEnumerator AttackCor()
     {
         _isAttack = true;
         float elapsedTime = 0f;
-        float attackTime = 3f;
+        float attackTime = 2f;
         Animator.SetBool(_attackName, true);
         while (elapsedTime < attackTime)
         {
@@ -26,5 +28,20 @@ public class DevilAttack : EnemyAttack
         }
         Animator.SetBool(_attackName, false);
         _isAttack = false;
+        _canAttack = false;
+        StartCoroutine(Reload());
+    }
+
+    private IEnumerator Reload()
+    {
+        float elapsedTime = 0f;
+        float reloadTime = 2f;
+
+        while (elapsedTime < reloadTime)
+        {
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        _canAttack = true;
     }
 }
