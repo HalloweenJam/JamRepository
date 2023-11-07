bb74b1221cbf69cf9822b7f1a27e2aa10f6581f2
Ivan Frolov <xMrFinlandx@gmail.com>
2023-11-07 11:44:27 +0300

    пофиксил анимации пуль

---
+++ ProjectHalloweenJam/Assets/Scripts/Editor/OptionalPropertyDrawer.cs
@@ -0,0 +1,26 @@
+﻿using Core.Classes;
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+    [CustomPropertyDrawer(typeof(OptionalProperty<>))]
+    public class OptionalPropertyDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect pos, SerializedProperty property, GUIContent label)
+        {
+            var valueProperty = property.FindPropertyRelative("_value");
+            var enabledProperty = property.FindPropertyRelative("_enabled");
+
+            pos.width -= 24;
+            EditorGUI.BeginDisabledGroup(!enabledProperty.boolValue);
+            EditorGUI.PropertyField(pos, valueProperty, label, true);
+            EditorGUI.EndDisabledGroup();
+
+            pos.x += pos.width + 24;
+            pos.width = pos.height = EditorGUI.GetPropertyHeight(enabledProperty);
+            pos.x -= pos.width;
+            EditorGUI.PropertyField(pos, enabledProperty, GUIContent.none);
+        }
+    }
+}
