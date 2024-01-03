c206f21568a19f52f438e4c05dd56360ac2b4ead
Ivan Frolov <xMrFinlandx@gmail.com>
2024-01-03 18:23:59 +0300

    трейл после рывка

---
+++ ProjectHalloweenJam/Assets/Scripts/Visuals/AfterImageController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Visuals
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class AfterImageController : MonoBehaviour
+    {
+        [SerializeField] private float _spawnDelay = .1f;
+        [SerializeField] private float _startOpacity = .7f;
+        [SerializeField] private float _length = 1f;
+        [Space(12)]
+        [SerializeField] private SpriteRenderer _playerRenderer;
+        [SerializeField] private SpriteRenderer _childRenderer;
+        
+        private Coroutine _spawnCoroutine;
+
+        public void Play(bool autoDisable = true)
+        {
+            Stop();
+            _spawnCoroutine = StartCoroutine(Spawn());
+            
+            if (autoDisable)
+                StartCoroutine(StopStopCoroutine());
+        }
+
+        private IEnumerator StopStopCoroutine()
+        {
+            yield return new WaitForSeconds(_length);
+            Stop();
+        }
+
+        public void Stop()
+        {
+            if (_spawnCoroutine != null)
+                StopCoroutine(_spawnCoroutine);
+        }
+
+        private IEnumerator Spawn()
+        {
+            while (true)
+            {
+                VisualsManager.Instance.CreateAfterImage(_playerRenderer, _childRenderer, _startOpacity);
+                yield return new WaitForSeconds(_spawnDelay);
+            }
+        }
+    }
+}
---
+++ ProjectHalloweenJam/Assets/Scripts/Visuals/VisualsManager.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Managers;
+using UnityEngine;
+using UnityEngine.Pool;
+
+namespace Visuals
+{
+    public class VisualsManager : Singleton<VisualsManager>
+    {
+        [SerializeField] private AfterImageObject _spriteRendererHolder;
+
+        private readonly List<AfterImageObject> _afterImageObjects = new();
+        private ObjectPool<AfterImageObject> _spritePool;
+
+        public void CreateAfterImage(SpriteRenderer playerRenderer, SpriteRenderer childRenderer, float startOpacity)
+        {
+            var afterImageObject = _spritePool.Get();
+            var child = afterImageObject.ChildRenderer;
+            var player = afterImageObject.PlayerRenderer;
+            
+            afterImageObject.gameObject.layer = playerRenderer.gameObject.layer;
+            
+            CopyRenderer(playerRenderer, player, startOpacity, 10);
+            CopyRenderer(childRenderer, child, startOpacity, 12);
+            
+            afterImageObject.gameObject.SetActive(true);
+            
+            AddToOpacityQueue(afterImageObject);
+        }
+
+        private static void CopyRenderer(SpriteRenderer toCopy, SpriteRenderer result, float startOpacity, int sortingOrder)
+        {
+            var copyTransform = toCopy.transform;
+            var resultTransform = result.transform;
+            
+            result.sprite = toCopy.sprite;
+            result.sortingLayerID = toCopy.sortingLayerID;
+            result.sortingLayerName = toCopy.sortingLayerName;
+            result.sortingOrder = toCopy.sortingOrder - sortingOrder;
+            result.flipX = toCopy.flipX;
+            result.flipY = toCopy.flipY;
+
+            resultTransform.rotation = copyTransform.rotation;
+            resultTransform.position = copyTransform.position;
+            resultTransform.localScale = copyTransform.localScale;
+
+            var color = result.color;
+            color.a = startOpacity;
+            result.color = color;
+        }
+
+        private void AddToOpacityQueue(AfterImageObject spriteRenderer)
+        {
+            _afterImageObjects.Add(spriteRenderer);
+        }
+
+        private void Start()
+        {
+            _spritePool = new ObjectPool<AfterImageObject>(CreateSprite);
+        }
+
+        private AfterImageObject CreateSprite() => Instantiate(_spriteRendererHolder, transform);
+
+        private void Update()
+        {
+            var deltaTime = Time.deltaTime;
+
+            var toDestroy =
+                _afterImageObjects.Where(afterImageObject => !(afterImageObject.DecreaseAlpha(deltaTime * .5f) > 0));
+
+            foreach (var spriteRenderer in toDestroy)
+            {
+                spriteRenderer.gameObject.SetActive(false);
+                
+                _afterImageObjects.Remove(spriteRenderer);
+                _spritePool.Release(spriteRenderer);
+            }
+        }
+    }
+}
